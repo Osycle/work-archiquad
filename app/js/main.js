e0262aae2772daa67eafefd0eba0ff258228c89a
Джанаев Азат <osycle@gmail.com>
2019-12-03 12:39:34 +0500

    03.12.2019

--- app/js/main.js
+++ app/js/main.js
@@ -445,7 +445,77 @@
 		});
 		$(window).trigger("scroll");
 
+		//Прелоудер
+		window.preLoader = {
+		  preBox: "#prebox",
+		  enter: true,
+		  status: $("#prebox").hasClass("in"),
 
+		  preToggle: function(bool, func) {
+
+		    var endtime = 600;
+		    //if (!this.enter) return;
+		    if (typeof func === "function")
+		      setTimeout(function() {
+		        func();
+		      }, endtime);
+		    var preBox = $(this.preBox);
+		    if (bool || this.status) {
+		    	setTimeout(function(){
+			      preBox.removeClass("in");
+		    	}, endtime)
+		     } else {
+			     	preBox
+			      	.show()
+			      	.addClass("in")
+			      	.find(".box-content");
+				      setTimeout(function() {
+					      $(preLoader.preBox).fadeOut();
+				      }, 1000);
+		     }
+		    return (this.status = !this.status);
+
+		  },
+
+		  preImg: function(img) {
+		    var images = img || document.images,
+		      imagesTotalCount = images.length,
+		      imagesLoadedCount = 0,
+		      preloadPercent = $(".percent").text("0 %");
+		      
+		    if (imagesTotalCount == 0) {
+		      preOnload();
+		      $(preloadPercent).text("100 %");
+		    }
+
+		    for (var i = 0; i < imagesTotalCount; i++) {
+		      var image_clone = new Image();
+		      image_clone.onload = image_loaded;
+		      image_clone.onerror = image_loaded;
+		      image_clone.src = images[i].src;
+		    }
+
+		    function preOnload() {
+		      onLoaded();
+		      preLoader.preToggle();
+		    }
+
+		    function image_loaded() {
+		      imagesLoadedCount++;
+
+		      var per = (100 / imagesTotalCount * imagesLoadedCount) << 0;
+
+		      setTimeout(function() {
+		        //console.log(per);
+		        $(preloadPercent).text(per + "%");
+		        $(".text-shadow").css("left", per + "%")
+		      }, 500);
+
+		      if (imagesLoadedCount >= imagesTotalCount) preOnload();
+		    }
+		  }
+		};
+		preLoader.preImg();
 
 	});
 })(jQuery);
